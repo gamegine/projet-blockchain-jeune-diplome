584e258bea484e344227259e8c9e2407d3685a64
gamegine <gamegine@protonmail.com>
2020-06-05 14:03:41 +0200

    truffle contract migrate

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,28 @@ const Banque = artifacts.require("Banque");
 const Token = artifacts.require("Token");
 const JeuneDiplome = artifacts.require("JeuneDiplome");
 
-module.exports = function (deployer) {
+module.exports = function (deployer, network, accounts) {
   deployer.deploy(SimpleStorage);
+  deployer
+    .deploy(Token, 1000)
+    .then(async () => {
+      //get instrance token
+      let instance = await Token.deployed();
+      //give 500 token to accounts 2
+      await instance.transfer(accounts[1], 500);
+    })
+    .then(() => deployer.deploy(JeuneDiplome, Token.address))
+    .then(async () => {
+      //get instrance token
+      let instance = await Token.deployed();
+      //approve JeuneDiplome to use 500 token of own
+      await instance.approve(JeuneDiplome.address, 500);
+    })
+    .then(() => deployer.deploy(Banque, Token.address))
+    .then(async () => {
+      //get instrance token
+      let instance = await Token.deployed();
+      //approve Banque to use 500 token of own
+      await instance.approve(Banque.address, 500);
+    });
 };
